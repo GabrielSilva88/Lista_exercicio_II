00f26f5537417fcd1f3fe3dcc447f68bb31acf6d
unknown <gabrielmg.silva@outlook.com>
2020-04-03 14:27:44 -0300

    Exercicio Aula dois e tres

---
+++ Documents/AulaDois/_javascript/funcoes.js
@@ -0,0 +1,11 @@
+
+/*function mudaFoto(foto) {
+    document.getElementById("icon").src = foto;
+}*/
+
+funtion cal_total(){
+    var qtd = parseInt(document.getElementById('cqtd').value);
+    tot = qtd *1500;
+    document.getElementById('ctot').value = tot;
+}
+
